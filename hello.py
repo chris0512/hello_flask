0521f6126c489422cca52b119924c302b7964fb3
Chris0512 <kyang3200@gmail.com>
2021-06-28 18:32:37 +0800

    adding decorator function

--- hello.py
+++ hello.py
@@ -6,12 +6,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1 style='text-align:center'>Hello, World!</h1>" \
+           "<p>This is a paragraph.</p>" \
+           "<img src='https://media.giphy.com/media/mwP7xG8I3qdOeBvOB2/giphy.gif' width=200>"
+
+
+def make_bold(function):
+    def wrapper_function():
+        return "<b>" + function() + "</b>"
+    return wrapper_function
+
+
+def make_emphasis(function):
+    def wrapper_function():
+        return "<em>" + function() + "</em>"
+    return wrapper_function
+
+
+def make_underlined(function):
+    def wrapper_function():
+        return "<u>" + function() + "</u>"
+    return wrapper_function
 
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
-    return "bye"
+    return "Bye!"
 
 
 @app.route("/username/<name>/<int:number>")
